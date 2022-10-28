0751f502b7d33177423224b305528b9ed657366c
Bernardo Pinheiro <berpinheiroo@gmail.com>
2022-10-27 21:26:33 -0300

    Buscar pelo nome

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-var id = []
-var nome = []
-var preco = []
-var avaliacao = []
+var ids = []
+var nomes = []
+var precos = []
+var avaliacoes = []
 var continuar = "s"
 var contador = 0
 
 function DesejaContinuar() {
     while (continuar == "s") {
-        id[contador] = prompt("Insira o id do produto ", [contador])
-        nome[contador] = prompt("Insira o nome do produto ", [contador])
-        preco[contador] = prompt("Insira o preco do produto ", [contador])
-        avaliacao[contador] = prompt("Insira uma avaliacao de 1-5 ao produto ", [contador])
-        console.log("ID:", id[contador], "Nome:", nome[contador], "Preco:", preco[contador], "Avaliacao:", avaliacao[contador])
+        ids[contador] = prompt("Insira o id do produto ", [contador])
+        nomes[contador] = prompt("Insira o nome do produto ", [contador])
+        precos[contador] = prompt("Insira o preco do produto ", [contador])
+        avaliacoes[contador] = prompt("Insira uma avaliacao de 1-5 ao produto ", [contador])
+        console.log("ID:", ids[contador], "Nome:", nomes[contador], "Preco:", precos[contador], "Avaliacao:", avaliacoes[contador])
         contador++
         continuar = prompt("Deseja adicionar mais produtos? Digite 's' para sim?")
     }
 }
 DesejaContinuar()
 
-var index = 0
 function ProcurarID() {
-    index = parseInt(prompt("Insira o ID do produto que deseja buscar"))
-    console.log("O produto com o ID", index, "tem nome:", nome[index], ", preco", preco[index], "R$", "e avaliacao:", avaliacao[index])
+    var index = parseInt(prompt("Insira o ID do produto que deseja buscar"))
+    console.log("O produto com o ID", index, "tem nome:", nomes[index], ", preco", precos[index], "R$", "e avaliacao:", avaliacoes[index])
 }
 ProcurarID()
+
+
+function ProcurarNome(){
+    var infoNome = prompt("Insira o nome do produto que deseja buscar")
+    for(i = 0; i < nomes.length; i++){
+        if(infoNome == nomes[i])
+        console.log("O produto com o nome", nomes[i], "tem ID:", ids[i], ", preco", precos[i], "R$", "e avaliacao:", avaliacoes[i])
+    }
+}
+ProcurarNome()
