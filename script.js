0657b26227a6e09ec0d77dd2892d06ce78f380a0
Bernardo Pinheiro <berpinheiroo@gmail.com>
2022-10-28 21:04:50 -0300

    Adicionando funcao ordenar pelo preco

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ var contador = 0
 
 function DesejaContinuar() {
     while (continuar == "s") {
-        ids[contador] = prompt("Insira o id do produto ", [contador])
+        ids[contador] = parseInt(prompt("Insira o id do produto ", [contador]))
         nomes[contador] = prompt("Insira o nome do produto ", [contador])
-        precos[contador] = prompt("Insira o preco do produto ", [contador])
+        precos[contador] = parseInt(prompt("Insira o preco do produto ", [contador]))
         avaliacoes[contador] = prompt("Insira uma avaliacao de 1-5 ao produto ", [contador])
         console.log("ID:", ids[contador], "Nome:", nomes[contador], "Preco:", precos[contador], "Avaliacao:", avaliacoes[contador])
         contador++
@@ -35,17 +35,16 @@ function ProcurarNome() {
 
 var maiorNumero = 0
 var idsOrdenados = []
-var contadorIds = 0
 function OrdemID() {
     for (var index = 0; index < ids.length; index++) {
-    maiorNumero = ids[index]
+        maiorNumero = ids[index]
         for (var index2 = 0; index2 < ids.length; index2++) {
             if (maiorNumero < ids[index2]) {
                 maiorNumero = ids[index2]
             }
         }
-        for(var index3 = 0; index3 < ids.length; index3++){
-            if(maiorNumero == ids[index3]){
+        for (var index3 = 0; index3 < ids.length; index3++) {
+            if (maiorNumero == ids[index3]) {
                 idsOrdenados[index] = ids[index3]
                 ids[index3] = 0
             }
@@ -55,3 +54,25 @@ function OrdemID() {
 
 OrdemID()
 console.log(idsOrdenados)
+
+
+var maiorPreco = 0
+var precosOrdenados = []
+function OrdemPreco() {
+    for (var index = 0; index < precos.length; index++) {
+        maiorPreco = precos[index]
+        for (var index2 = 0; index2 < precos.length; index2++) {
+            if (precos[index2] > maiorPreco) {
+                maiorPreco = precos[index2]
+            }
+        }
+        for (var index3 = 0; index3 < precos.length; index3++) {
+            if (maiorPreco == precos[index3]) {
+                precosOrdenados[index] = precos[index3]
+                precos[index3] = 0
+            }
+        }
+    }
+}
+OrdemPreco()
+console.log(precosOrdenados)
