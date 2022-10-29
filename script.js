0983b3c70b7e1c7d3d195f03e4c0747d36a818aa
Bernardo Pinheiro <berpinheiroo@gmail.com>
2022-10-28 22:25:23 -0300

    Adicionando funcao ordenar por avaliacao e arrumando id

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ function DesejaContinuar() {
         ids[contador] = parseInt(prompt("Insira o id do produto ", [contador]))
         nomes[contador] = prompt("Insira o nome do produto ", [contador])
         precos[contador] = parseInt(prompt("Insira o preco do produto ", [contador]))
-        avaliacoes[contador] = prompt("Insira uma avaliacao de 1-5 ao produto ", [contador])
-        console.log("ID:", ids[contador], "Nome:", nomes[contador], "Preco:", precos[contador], "Avaliacao:", avaliacoes[contador])
+        avaliacoes[contador] = parseInt(prompt("Insira uma avaliacao de 1-5 ao produto ", [contador]))
         contador++
         continuar = prompt("Deseja adicionar mais produtos? Digite 's' para sim?")
     }
@@ -33,6 +32,9 @@ function ProcurarNome() {
     }
 }
 
+var precosId = []
+var avaliacoesId = []
+var nomesId = []
 var maiorNumero = 0
 var idsOrdenados = []
 function OrdemID() {
@@ -46,14 +48,18 @@ function OrdemID() {
         for (var index3 = 0; index3 < ids.length; index3++) {
             if (maiorNumero == ids[index3]) {
                 idsOrdenados[index] = ids[index3]
+                precosId[index] = precos[index3]
+                avaliacoesId[index] = avaliacoes[index3]
+                nomesId[index] = nomes[index3]
                 ids[index3] = 0
+                console.log("ID:", idsOrdenados[index], " Nome:", nomesId[index], " Preco:", precosId[index], "Avaliacao:", avaliacoesId[index])
+                maiorNumero = 100000000
             }
         }
     }
 }
-
 OrdemID()
-console.log(idsOrdenados)
+
 
 
 var maiorPreco = 0
@@ -73,6 +79,29 @@ function OrdemPreco() {
             }
         }
     }
+    console.log(precosOrdenados)
 }
 OrdemPreco()
-console.log(precosOrdenados)
+
+
+var maiorAvaliacao = 0
+var avaliacoesOrdenadas = []
+function OrdemAvaliacao() {
+    for (var index = 0; index < avaliacoes.length; index++) {
+        maiorAvaliacao = avaliacoes[index]
+        for (var index2 = 0; index2 < avaliacoes.length; index2++) {
+            if (avaliacoes[index2] > maiorAvaliacao) {
+                maiorAvaliacao = avaliacoes[index2]
+            }
+        }
+        for (var index3 = 0; index3 < avaliacoes.length; index3++) {
+            if (maiorAvaliacao == avaliacoes[index3]) {
+                avaliacoesOrdenadas[index] = maiorAvaliacao
+                avaliacoes[index3] = -1
+                maiorAvaliacao = 6
+            }
+        }
+    }
+    console.log(avaliacoesOrdenadas)
+}
+OrdemAvaliacao()
